14a44c7680e3cfa167f9f9e40d6e4e8c69f3e2f0
hepingpeace <jiangmen28@gmail.com>
2022-07-27 11:10:56 +0900

    新增index.py

---
+++ py/index.py
@@ -0,0 +1,7 @@
+echo "# python_-" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:hepingpeace/python_-.git
+git push -u origin main
